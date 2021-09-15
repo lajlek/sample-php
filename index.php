12ddcf4d17fa52611d21bae49b9193067b835191
lajlek <48017306+lajlek@users.noreply.github.com>
2021-09-15 06:35:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,10 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Cowsayphp\Farm;
 
-header('Content-Type: text/plain');
-
+//header('Content-Type: text/plain');
+echo "<pre>";
+var_dump($_SERVER);
+exit;
 $text = "Set a message by adding ?message=<message here> to the URL";
 if(isset($_GET['message']) && $_GET['message'] != '') {
 	$text = htmlspecialchars($_GET['message']);
